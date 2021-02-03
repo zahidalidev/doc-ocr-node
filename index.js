cf40bdaca1ddd3f8b20b6f0936a3ae19a2b3b19f
zahidalidev <m.zahidalidev@gmail.com>
2021-02-03 17:23:00 +0500

    extendTimeoutMiddleware

--- index.js
+++ index.js
@@ -1,10 +1,7 @@
 const express = require('express')
 const cors = require('cors')
-// const timeout = require('connect-timeout')
 
 const app = express()
-// app.use(timeout('90s'))
-// app.use(haltOnTimedout)
 
 app.use(cors({ origin: true, credentials: true }))
 
@@ -14,10 +11,8 @@ app.set('port', (process.env.PORT || 5000))
 
 require('./startup/routes')(app)
 
-// function haltOnTimedout(req, res, next) {
-//     if (!req.timedout) next()
-// }
-
 app.listen(app.get('port'), function () {
     console.log(`Listing on port ${app.get('port')}...`)
 })
+
+
